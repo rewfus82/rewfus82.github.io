4ffe880849691daae56e7e85f234ec35bed0a996
rewfus82 <rcmc82@gmail.com>
2022-04-05 11:54:27 -0500

    Update DrinkModel.java

--- CoffeeShop-Refactor/coffee-shop/src/app/models/DrinkModel.java
+++ CoffeeShop-Refactor/coffee-shop/src/app/models/DrinkModel.java
@@ -3,22 +3,7 @@ package app.models;
 public class DrinkModel extends MenuItemModel{
     final String type = "drink";
     protected boolean caffeine = false;
-
-    public DrinkModel() {
-        super();
-        this.setID();
-    }
-
-    public DrinkModel(String name, String description, double cost) {
-        super(name, description, cost);
-        this.setID();
-    }
-
-    public void setID() {
-        this.ID = "D-" + idg.generate();
-    }
-    
-
-
-
+    public DrinkModel() {super();this.setID();}
+    public DrinkModel(String name, String description, double cost) {super(name, description, cost);this.setID();}
+    public void setID() {this.ID = "D-" + idg.generate();}
 }
