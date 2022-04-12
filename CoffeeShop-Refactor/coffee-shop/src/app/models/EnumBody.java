7b8e3981edea7911d6a74183b90dc327d5be5c19
rewfus82 <rcmc82@gmail.com>
2022-04-12 09:27:05 -0500

    Update EnumBody.java

--- CoffeeShop-Refactor/coffee-shop/src/app/models/EnumBody.java
+++ CoffeeShop-Refactor/coffee-shop/src/app/models/EnumBody.java
@@ -1,28 +1,10 @@
 package app.models;
 
 public interface EnumBody {
-
-    public enum CustomerField {
-        CUSTOMERID, FIRSTNAME, LASTNAME, CITY, STATE, STREETADDRESS
-    }
-
-    public enum Tender {
-        TOTAL, PAID, CHANGE
-    }
-
-    public enum TenderType {
-        CASH, CREDIT, DEBIT, CHECK, GIFTCARD, BTC, ETH, DOGE
-    }
-
-    public enum Points {
-        ADD, SUB, SET
-    }
-
-    public enum PointsGet {
-        GET
-    }
-    
-    public enum MenuItem {
-        NAME, DESC
-    }
+    public enum CustomerField { CUSTOMERID, FIRSTNAME, LASTNAME, CITY, STATE, STREETADDRESS }
+    public enum Tender { TOTAL, PAID, CHANGE }
+    public enum TenderType { CASH, CREDIT, DEBIT, CHECK, GIFTCARD, BTC, ETH, DOGE }
+    public enum Points { ADD, SUB, SET }
+    public enum PointsGet { GET }
+    public enum MenuItem { NAME, DESC }
 }
